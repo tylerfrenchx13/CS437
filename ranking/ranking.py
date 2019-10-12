4bc2f72836c51f9a2bd64ac3125145717c81bd59
Nicholas Petitmermet <darthvader121@gmail.com>
2019-10-12 14:19:34 -0600

    Getting the idf for ranking works

--- ranking/ranking.py
+++ ranking/ranking.py
@@ -6,10 +6,12 @@ def findCandidates(file, query):
     fileReader = csv.reader(fileReader, delimiter='\t')
     queryArray = query.split()
     candidates = []
+    wordOccurrence = {}
     foundOne = False
     for row in fileReader:
         for word in queryArray:
             if row[0] == word:
+                wordOccurrence[word] = len(row)-1;
                 newDocs = [];
                 for i in range(len(row)-1):
                     if foundOne == False:
@@ -21,5 +23,30 @@ def findCandidates(file, query):
                 if foundOne == True:
                     candidates = [value for value in candidates if value in newDocs]
                 foundOne=True
-    return candidates
-findCandidates("app/term_doc_freq.tsv", "aborigin 1992")
+    return [candidates,wordOccurrence]
+
+def rankMeUpScotty(metadataFile,termDocFile, docTermFreq, query, collectionSize):
+    candidates = findCandidates(termDocFile,query)
+    rank = ranking(candidates, metadataFile, termDocFile, docTermFreq ,query,collectionSize)
+
+def ranking(candidates, metadata, termDocFile, docTermFreq, query,collectionSize):
+    candidateRank = {}
+    queryList = query.split()
+    for candidate in candidates[0]:
+        summation = [];
+        totalVal = 0
+        for word in queryList:
+            val = 0
+            IDF = 0
+            TF = 0;
+            #This represents the IDF of the document.
+            fractionalVal = collectionSize/candidates[1][word];
+            IDF = log(fractionalVal,2);
+            #This represents the TF of the document.
+            summation.append(val)
+        for val in summation:
+            totalVal+=val;
+        candidateRank[candidate] = totalVal;
+    return candidateRank
+
+rankMeUpScotty("app/metadata.tsv", "app/term_doc_freq.tsv", "temp", "aborigin 1992",100000)
