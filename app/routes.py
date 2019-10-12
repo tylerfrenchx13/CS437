62e90765d5081c7500a9631ac5d8f5ca633116b8
Brody Downs <brodydowns@u.boisestate.edu>
2019-10-12 14:26:09 -0600

    Query Suggestions implemented in flask app

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from flask import render_template
 from app import app
 from .TextManager import TextManager
+from .QuerySuggestions import QuerySuggestions
+from flask import jsonify
 
 @app.route('/')
 def index():
-	TextManager.processText()
-	
-	
-	#return render_template('index.html', data=data)
+	#TextManager.processText()
+	return render_template('index.html', data=None)
+
+@app.route('/query_suggestion/<string:query>', methods=['GET'])
+def query_suggestion(query):
+    #print(QuerySuggestions.GetCandidates('tiger'))
+	return jsonify(QuerySuggestions.GetCandidates(query))
