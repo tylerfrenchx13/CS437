ce4d0fde814cbe9da71e78d7eee446fc4032d3a0
tylerfrench13 <tylerfrench@u.boisestate.edu>
2019-11-09 13:51:30 -0700

    reduced index size

--- text_processing/processText2.py
+++ text_processing/processText2.py
@@ -4,16 +4,23 @@ import re
 import pandas as pd
 import string
 from collections import Counter
+import matplotlib.pyplot as plt
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import TweetTokenizer
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 words = set(nltk.corpus.words.words())
 stopWords = set(stopwords.words('english'))
 ps = PorterStemmer()
+tknzr = TweetTokenizer()
 #table = str.maketrans({key: None for key in string.punctuation})
 import sys
-myStopwords = {"``", "''", "'s", "--", "...", "'ve", "'ll", "n't", "'re", "'in"}
+myStopwords = {"--", "...", "’", "–", "”", "“", "…", "•", "—", "‘"}
+zipfsWords = {
+	"game", "play", "s", "world", "new", "use", "time", "featur", "player", "one",
+	"level", "differ", "take", "get", "make", "way", "uniqu", "find", "enemi", "challeng"
+}
 #csv.field_size_limit(sys.maxsize)
 csv.field_size_limit(2**31-1)
 
@@ -51,15 +58,26 @@ def createDocTerm():
 				dest.write("{0}:{1}".format(key, countFrequency[key]))
 			dest.write("\n")
 
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
 
 def tokenize(content):
-	lowered = content.lower()
-	tokenized = word_tokenize(lowered)
+	tokenized = content.lower()
+	tokenized = tknzr.tokenize(tokenized)
+	tokenized = [token for token in tokenized if isEnglish(token)]
+	tokenized = [token for token in tokenized if not token in string.punctuation]
+	"""tokenized = word_tokenize(lowered)"""
 	""" and not token in myStopwords and token not in string.punctuation and isEnglish(token)"""
-	noStopwords = [token for token in tokenized if not token in stopWords]
-	stemmed = [ps.stem(word) for word in noStopwords]
-	final = [re.sub('[^A-Za-z0-9]+', '', token) for token in stemmed]
-	return final
+	tokenized = [token for token in tokenized if not token in stopWords]
+	tokenized = [ps.stem(word) for word in tokenized]
+	"""final = [re.sub('[^A-Za-z0-9]+', '', token) for token in stemmed]"""
+	"""final = [token for token in stemmed if not token in zipfsWords]"""
+	return tokenized
 
 def createTermDoc():
 	with open("tokenDocNEW.csv", "r", encoding="utf8") as src:
@@ -125,20 +143,88 @@ def cleanText():
 				appid = row[1][0]
 				if appid in documents:
 					description = row[1][1]
-					cleanr = re.compile('<.*?>')
-					description = re.sub(cleanr, '', description)
-					description = description.replace("\n", "")
+					htmlreg = re.compile('<.*?>')
+					description = re.sub(htmlreg, ' ', description)
+					urlreg = re.compile('((http|https|www\.)\S*(\.com)*)|((http|https|www\.)*\S*(\.com))', re.IGNORECASE)
+					description = re.sub(urlreg, ' ', description)
+					description = description.replace("\n", " ")
 					description = description.replace("\t", " ")
 					title = documents[appid]
 
 					dest.write('{0},"{1}","{2}"\n'.format(index, title, description))
 
+def createZipfs():
+	with open("tokenDocNEW.csv", "r", encoding="utf8") as src:
+		reader = csv.reader(src)
+
+		tokenCounts = {}
+		for index, row in enumerate(reader):
+			print(index)
+			count = Counter(row)
+			for token in count:
+				if token not in tokenCounts:
+					tokenCounts[token] = 0
+				tokenCounts[token] += 1
+
+		tokenCounts = sorted(tokenCounts.items(), key = lambda kv:(kv[1], kv[0]), reverse=True)
+
+		with open("zipfs.tsv", "w", encoding="utf8") as dest:
+			for token in tokenCounts:
+				dest.write("{0}\t{1}\n".format(token[0], token[1]))
+
+def plotZipfs():
+	frequencies = []
+	with open('zipfs.tsv', 'r', encoding="utf8") as src:
+		reader = csv.reader(src, delimiter='\t')
+		for row in reader:
+			frequencies.append(int(row[1]))
+	plt.loglog(range(1, len(frequencies)+1), frequencies, 'bo', markersize=2.0)
+	plt.ylabel("Frequency")
+	plt.xlabel("Rank")
+	plt.show()
+
+def createHeaps():
+	with open("tokenDocNEW.csv", "r", encoding="utf8") as src, open("heaps.tsv", "w", encoding="utf8") as dest:
+		reader = csv.reader(src)
+
+		tokens = {}
+		vocabSize = 0
+		for index, row in enumerate(reader):
+			print(index)
+			count = Counter(row)
+			for token in count:
+				if token not in tokens:
+					tokens[token] = 0
+					vocabSize += 1
+
+			dest.write("{0}\n".format(vocabSize))
+
+def plotHeaps():
+	vocab = []
+	with open('heaps.tsv', 'r', encoding="utf8") as f:
+		reader = csv.reader(f, delimiter='\t')
+		for row in reader:
+			vocab.append(int(row[0]))
+
+	plt.plot(range(1, len(vocab)+1), vocab, 'bo', markersize=2.0)
+
+	plt.ylabel("Total Vocabulary")
+	plt.xlabel("Document")
+	plt.show()
+
 def main():
 	
 	#cleanText()
-	#tokenizeText()
-	#createTermDoc()
-	#createDocTerm()
+	tokenizeText()
+	
+	createTermDoc()
+	createDocTerm()
 	createMetadata()
+	
+	createZipfs()
+	#plotZipfs()
+
+	createHeaps()
+	#plotHeaps()
 
 main()
